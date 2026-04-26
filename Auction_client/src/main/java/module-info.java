173359338c8ef4f34f66b16409035b46f6ecd679
Lekeduc <duc11072k7@gmail.com>
2026-04-26 22:48:33 +0700

    Revert "merge: sync KeDuc with develop and resolve gitignore conflict"
    This reverts commit e2a71778ce60d1da26c8b673034d08968949029b, reversing
    changes made to 02a0f9a955eef4973b817b6f2fc3fb16b6c20a18.

--- Auction_client/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.auction.client {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.auction.shared; // Gọi sang Shared
-
-    opens com.auction.client to javafx.fxml;
-    exports com.auction.client;
-}
