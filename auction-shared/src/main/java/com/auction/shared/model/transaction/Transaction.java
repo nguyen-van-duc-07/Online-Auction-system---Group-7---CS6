02a0f9a955eef4973b817b6f2fc3fb16b6c20a18
Lekeduc <duc11072k7@gmail.com>
2026-04-26 21:33:20 +0700

    feat (Transaction): Complete BidTransaction.java, add detailed logic description for PrizedTransaction.java, and apply Lombok for Transaction.java

--- auction-shared/src/main/java/com/auction/shared/model/transaction/Transaction.java
+++ auction-shared/src/main/java/com/auction/shared/model/transaction/Transaction.java
@@ -15,3 +15,10 @@ public abstract class Transaction {
     }
     abstract public void showInfo();
 }
+
+/*Phân tích cấu trúc hiện tại
+Abstract Transaction: Có fromId và receiveId là chuẩn. Mọi giao dịch đều cần thực thể gửi và nhận.
+WalletTransaction: Dùng để ghi lại lịch sử Nạp/Rút/Chuyển tiền. Rất tốt khi có TransactionType để phân loại.
+BidTransaction: Dùng cho lịch sử đặt giá. Việc tách riêng này giúp bạn dễ dàng truy vấn "Lịch sử đấu giá của
+món đồ X" mà không bị lẫn với lịch sử nạp tiền.
+PrizedTransaction: Đây là giao dịch khi cuộc đấu giá kết thúc (thanh toán hóa đơn thắng cuộc).*/
