623ad91dc4e8fd4cad69510d866b793b0d41abb3
Nguyễn Đức <ducnguyen03012007@gmail.com>
2026-04-28 04:26:12 +0700

    feat: adding result screen and initialize server

---
+++ Auction_server/src/main/java/com/auction/server/ClientHandler.java
@@ -0,0 +1,44 @@
+package com.auction.server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(clientSocket.getInputStream());
+
+            Object request;
+            while ((request = in.readObject()) != null) {
+                // Phân loại yêu cầu
+                // Viết tiếp code ở đây
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (in != null) in.close();
+            if (out != null) out.close();
+            if (clientSocket != null) clientSocket.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Auction_server/src/main/java/com/auction/server/Server.java
@@ -0,0 +1,29 @@
+package com.auction.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    public static int SERVER_PORT = 8080;
+    private static final ExecutorService pool = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            List<Socket> clientSockets = new ArrayList<>();
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                clientSockets.add(clientSocket);
+                ClientHandler task = new ClientHandler(clientSocket);
+                pool.execute(task);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
